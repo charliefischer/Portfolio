701cfddbe19091ef5bc2ce63e314b2b84d26e5e6
charlie <charlie@Charlies-MacBook-Pro.local>
2023-06-01 21:46:28 +0100

    first init with cms configured

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,19 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Layout from "../components/layout";
+import { getHomePageContent } from "../lib/api";
 
-export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index({ preview, cmsContent }) {
   return (
     <>
       <Layout preview={preview}>
-        <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
+        <h1>{cmsContent.text}</h1>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
+  const cmsContent = await getHomePageContent(preview);
   return {
-    props: { preview, allPosts },
-  }
+    props: { preview, cmsContent: cmsContent?.content?.[0] ?? null },
+  };
 }
