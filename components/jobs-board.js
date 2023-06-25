8937199a6d6fdd5cda40e64f0b096a7b4d0e4806
charlie <charlie@Charlies-MacBook-Pro.local>
2023-06-25 08:27:30 -0400

    jobs booard done

---
+++ components/jobs-board.js
@@ -0,0 +1,73 @@
+import { getHomePageContent } from "../lib/api";
+
+export default function JobsBoard({ preview, cmsContent }) {
+  const previousJobs = {
+    Illustrator: true,
+    Labourer: true,
+    Barman: true,
+    "Bicycle Courier": true,
+    "Web Developer": true,
+    Barista: true,
+    Designer: true,
+    Removals: true,
+    "Cable Installer": true,
+  };
+
+  const name = "Charlie Fischer";
+  const currentJobs = { Illustrator: true, "Web Developer": true };
+  const jobList = [
+    "Lift Engineer",
+    "Labourer",
+    "Private Investigator",
+    "Barman",
+    "Electrician",
+    "Charlie Fischer",
+    "Landlord",
+    "Illustrator",
+    "Bicycle Courier",
+    "Driving Instructor",
+    "Stuntman",
+    "Web Developer",
+    "Animal Breeder",
+    "Barista",
+    "Tarot Reader",
+    "Careers Advisor",
+    "Body Guard",
+    "Chimney Sweep",
+    "Designer",
+    "Butler",
+    "Bus Driver",
+    "Grave Digger",
+    "Removals",
+    "Funeral Director",
+    "Baker",
+    "Cage Fighter",
+    "Cable Installer",
+    "Optician",
+  ];
+  return (
+    <>
+        <div className="jobs-titles-grid flex flex-wrap justify-between">
+          {jobList.map((job) => (
+            <span
+              key={job}
+              className={`${previousJobs[job] ? "text-[#F17B0D]" : ""} ${
+                job === name
+                  ? "text-[white] bg-[#F17B0D] rounded-[16px] px-[4px]"
+                  : ""
+              } ${currentJobs[job] ? "text-[#F17B0D]" : ""} ${!currentJobs[job] && name !== job ? "opacity-[0.25]" : ""}`}
+            >
+              {job}
+            </span>
+          ))}
+        </div>
+    </>
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const cmsContent = await getHomePageContent(preview);
+  return {
+    props: { preview, cmsContent: cmsContent?.content?.[0] ?? null },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,25 @@
 import { useEffect } from "react";
 import Layout from "../components/layout";
 import { getHomePageContent } from "../lib/api";
+import JobsBoard from "../components/jobs-board"
 
 export default function Index({ preview, cmsContent }) {
-  useEffect(() => {
-    const h1 = document.querySelector('#title')
-    const tween = document.querySelector('.tween')
-    window.addEventListener('scroll', e => {
-      const pixels = window.pageYOffset
-      const fontWeight = pixels * 0.4 + 150
-      const fontWidth = pixels * 0.05 + 25
-      
-      h1.style.fontVariationSettings = `"wght" ${ fontWeight }`
-      tween.style.fontVariationSettings = `"wght" ${ fontWeight }, "wdth" ${ fontWidth }`
-      // , "wdth" ${fontWidth}
-    })
-  })
+  // useEffect(() => {
+  // const h1 = document.querySelector('#title')
+  // const tween = document.querySelector('.tween')
+  // window.addEventListener('scroll', e => {
+  //   const pixels = window.pageYOffset
+  //   const fontWeight = pixels * 0.4 + 150
+  //   const fontWidth = pixels * 0.05 + 25
+  //   h1.style.fontVariationSettings = `"wght" ${ fontWeight }`
+  //   tween.style.fontVariationSettings = `"wght" ${ fontWeight }, "wdth" ${ fontWidth }`
+  //   // , "wdth" ${fontWidth}
+  // })
+  // });
   return (
     <>
       <Layout preview={preview}>
-        <div className='empty' />
-        <div className="wrapper">
-          <h1 id='title'>Ch<span className='tween'>a</span>rlie Fischer</h1>
-          <p>Website currently under construction</p>
-        </div>
+        <JobsBoard />
       </Layout>
     </>
   );
