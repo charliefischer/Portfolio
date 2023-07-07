084cd25f12c6fbf089d4fc2b5b3b0e8a22833bb9
charlie <charlie.a.fischer@gmail.com>
2023-07-07 13:43:36 -0400

    icons added

--- components/creative-coding/grid.js
+++ components/creative-coding/grid.js
@@ -12,7 +12,7 @@ export default function Grid() {
     const scrollHandler = (e) => {
       e.preventDefault();
       e.stopPropagation();
-      console.log(e)
+      console.log(e);
       return false;
     };
 
@@ -24,7 +24,7 @@ export default function Grid() {
       document
         .querySelector(".creative-coding-wrappers")
         .removeEventListener("scroll", scrollHandler);
-    }
+    };
   });
   let activeIndex = 0;
   const moveTo = (direction = false) => {
@@ -36,18 +36,47 @@ export default function Grid() {
   };
   const components = [<Crazy />, <Zipper />, <Whirl />, <Sticks />, <Delay />];
   return (
-    <section className="creative-coding-wrappers relative mt-[25%]">
+    <section className="creative-coding-wrappers relative my-[120px]">
       <div
-        className="arrow right fixed top-[50%] right-[10%] z-10"
+        className="arrow right absolute top-[50%] right-[10%] z-10 cursor-pointer"
         onClick={() => moveTo(true)}
       >
-        Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 28 28"
+          width="32px"
+          className=""
+        >
+          <path
+            fill="#000"
+            d="M14 0A14 14 0 0 0 0 14a14 14 0 0 0 14 14 14 14 0 0 0 14-14A14 14 0 0 0 14 0Zm0 26A12 12 0 0 1 2 14 12 12 0 0 1 14 2a12 12 0 0 1 12 12 12 12 0 0 1-12 12Z"
+          />
+          <path
+            fill="#000"
+            d="m20.9 13.6-.2-.3-4-4a1 1 0 0 0-1.4 0 1 1 0 0 0 0 1.4l2.3 2.3H7c-.6 0-1 .4-1 1s.4 1 1 1h10.6l-2.3 2.3a1 1 0 0 0 0 1.4c.2.2.5.3.7.3.2 0 .5-.1.7-.3l4-4 .2-.3c.1-.3.1-.5 0-.8Z"
+          />
+        </svg>
       </div>
       <div
-        className="arrow left fixed top-[50%] left-[10%] z-10"
+        className="arrow left absolute top-[50%] left-[10%] z-10 rotate-180 cursor-pointer"
         onClick={() => moveTo()}
       >
-        Prev
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 28 28"
+          width="32px"
+        >
+          <path
+            fill="#000"
+            d="M14 0A14 14 0 0 0 0 14a14 14 0 0 0 14 14 14 14 0 0 0 14-14A14 14 0 0 0 14 0Zm0 26A12 12 0 0 1 2 14 12 12 0 0 1 14 2a12 12 0 0 1 12 12 12 12 0 0 1-12 12Z"
+          />
+          <path
+            fill="#000"
+            d="m20.9 13.6-.2-.3-4-4a1 1 0 0 0-1.4 0 1 1 0 0 0 0 1.4l2.3 2.3H7c-.6 0-1 .4-1 1s.4 1 1 1h10.6l-2.3 2.3a1 1 0 0 0 0 1.4c.2.2.5.3.7.3.2 0 .5-.1.7-.3l4-4 .2-.3c.1-.3.1-.5 0-.8Z"
+          />
+        </svg>
       </div>
       <div className="coding-container flex flex-nowrap snap-x relative left-[0px] transition-transform duration-[800ms] transform overflow-hiddenn">
         {components.map((comp, i) => {
