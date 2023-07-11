471b3502593a7d9870f9ba0dcf95bf91c27cde84
charlie <charlie.a.fischer@gmail.com>
2023-07-11 10:59:30 -0400

    grid works for mmultiple illustrationn posts

--- components/illustration-grid.js
+++ components/illustration-grid.js
@@ -1,29 +1,16 @@
-import { useEffect } from "react";
-import { getIllustrationContent } from "../lib/api";
 import Link from "next/link";
 
-export default function IllustrationGrid({ preview, contents }) {
-  // useEffect(() => {
-  // });
+export default function IllustrationGrid({ contents }) {
   return (
-    <div>
+    <div className="flex flex-wrap justify-center">
       {contents.map(illo => {
        return (
-          <Link href={`/${illo.urlPath}`} key={illo.urlPath}>
-            <img src={illo.navigationImage.url} className="pointer-events-none" />
+          <Link href={`/${illo.urlPath}`} key={illo.urlPath} className="flex w-[max(300px,33%)]">
+            <img src={illo.navigationImage.url} className="pointer-events-none width-[100%]" />
           </Link>
        ) 
       })}
-      {/* <div>{cmsContent.items[0]}</div> */}
     </div>
   );
 }
-
-// export async function getStaticProps({ preview = false }) {
-//   console.log(1)
-//   const cmsContent = await getIllustrationContent(preview);
-//   return {
-//     props: { preview, cmsContent: cmsContent ?? null },
-//   };
-// }
 
