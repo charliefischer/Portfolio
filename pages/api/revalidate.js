fe690a6b016bb8fd7aa58f58f845501a4b06338a
charlie <charlie.a.fischer@gmail.com>
2023-08-27 19:39:47 -0700

    comment on where to update revalidate added

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -16,6 +16,8 @@ export default async function handler(req, res) {
   try {
     // Note: if this fails to parse you may have forget to set the
     // "content-type" header correctly as mentioned here https://github.com/vercel/next.js/blob/canary/examples/cms-contentful/README.md#step-9-try-using-on-demand-revalidation
+
+    // TO DO - revalidatte on content updatet
     let postSlug = req.body.fields.slug['en-US']
 
     // revalidate the individual post and the home page
