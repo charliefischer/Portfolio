9f1bfbb20305e93944dc602c2b3e7b1202f12b7f
charlie <charlie.a.fischer@gmail.com>
2023-08-27 18:52:05 -0700

    stlye tweaks and added ff4

--- components/site-selector.js
+++ components/site-selector.js
@@ -35,7 +35,7 @@ export default function SiteSelector({ illoCmsContent }) {
       />
       <div
         style={{ left: leftValue + 100 + "%" }}
-        className="fixed transition-all duration-[400ms] w-[90%] mx-[5%] mt-[32px] top-[0] mob:hidden sm:block"
+        className="fixed transition-all duration-[400ms] w-[90%] mx-[5%] mt-[64px] top-[0] mob:hidden sm:block overflow-scroll h-[100%]"
       >
         <IllustrationGrid contents={illoCmsContent} />
       </div>
@@ -43,7 +43,7 @@ export default function SiteSelector({ illoCmsContent }) {
         style={{
           left: leftValue + "%",
         }}
-        className="fixed transition-all duration-[400ms] w-[90%] mx-[5%] mt-[32px] top-[0] mob:hidden sm:block"
+        className="fixed transition-all duration-[400ms] w-[90%] mx-[5%] mt-[64px] top-[0] mob:hidden sm:block overflow-scroll h-[100%]"
       >
         <WebDevGrid contents={[1, 2, 3]} />
       </div>
@@ -51,32 +51,32 @@ export default function SiteSelector({ illoCmsContent }) {
         onMouseEnter={() => handleHover("web", -20)}
         onMouseLeave={() => handleHover()}
         onClick={() => handleCLick("web")}
-        className="absolute transition-all translate-x-[-50%] top-[0] mob:hidden sm:block"
+        className="absolute transition-all translate-x-[-50%] top-[0] mob:hidden sm:block pt-[8px]"
         style={{
           left: hoveredOption === "illo" ? leftValue + 90 + "%" : "25%",
         }}
       >
-        Web Developer
+        <a className="h3">Web Developer</a>
       </div>
       <div className="flex flex-col items-center sm:hidden fixed top-[33%] left-[50%] translate-x-[-50%]">
         <Link href={`/web`} className="mb-[32px]">
-          Web Developer
+          <h3>Web Developer</h3>
         </Link>
         <Link href={`/illustration`}>
-          Illustrator
+          <h3>Illustrator</h3>
         </Link>
       </div>
       <div
         onMouseEnter={() => handleHover("illo", -80)}
         onMouseLeave={() => handleHover()}
         onClick={() => handleCLick("illo")}
-        className="absolute transition-all translate-x-[50%] top-[0] mob:hidden sm:block"
+        className="absolute transition-all translate-x-[50%] top-[0] mob:hidden sm:block pt-[8px]"
         style={{
           right:
             hoveredOption === "web" ? (elementClicked ? "-10%" : "10%") : "25%",
         }}
       >
-        Illustrator
+        <a className="h3">Illustrator</a>
       </div>
     </div>
   );
--- components/web-dev-grid.js
+++ components/web-dev-grid.js
@@ -4,7 +4,7 @@ import Crazy from "./creative-coding/crazy";
 
 export default function WebDevGrid({ contents, styleObj }) {
   return (
-    <div className="flex flex-wrap justify-center gap-[12px] web-dev-grid" style={styleObj} web-dev-grid>
+    <div className="flex flex-wrap justify-center gap-[12px] web-dev-grid h-min" style={styleObj}>
       <Link href={`/web/creative-coding`}>
         <Crazy gridMode={true} />
       </Link>
@@ -13,14 +13,21 @@ export default function WebDevGrid({ contents, styleObj }) {
         href={`https://github.com/charliefischer/esoteric-ensembles/`}
         style={{ maxWidth: "min(80%, 400px)", height: "min-content" }}
       >
-        <img src="/web-images/esoteric-ensembles.png" className="pointer-events-none" />
+        <img src="/web-images/teaser.gif" className="pointer-events-none" />
       </Link>
       <Link
         target="_blank"
         href={`https://cf-brux-editor.superhi.hosting/index.html`}
         style={{ maxWidth: "min(80%, 400px)", height: "min-content" }}
       >
-        <img src="/web-images/brux.png" className="pointer-events-none" />
+        <img src="/web-images/brux.gif" className="pointer-events-none" />
+      </Link>
+      <Link
+        target="_blank"
+        href={`https://food-from-4.vercel.app/`}
+        style={{ maxWidth: "min(80%, 400px)", height: "min-content" }}
+      >
+        <img src="/web-images/ff4.png" className="pointer-events-none" />
       </Link>
     </div>
   );
