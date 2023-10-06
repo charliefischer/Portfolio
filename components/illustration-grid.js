93535fad58e555b4bae531f2bfa465ea958dd5ba
charlie <charlie.a.fischer@gmail.com>
2023-10-06 11:02:53 -0700

    homepage restyle

--- components/illustration-grid.js
+++ components/illustration-grid.js
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function IllustrationGrid({ contents=[{url: "a"}, {url: "a"}], styleObj, isHorizontal=false }) {
   let wrapperClasses = "flex flex-wrap justify-center h-min"
+  let linkClasses = "flex w-[max(300px,33%)] hover:scale-110 transition-transform duration-400 mb-8"
   if(isHorizontal) {
     wrapperClasses = "flex overflow-scroll illo-grid-horizontal h-min"
+    linkClasses = "flex h-[200px] hover:scale-110 transition-transform duration-400 mb-8 shrink-0"
   }
+  
   return (
     <div className={wrapperClasses}>
       {contents?.map((illo) => {
@@ -12,7 +15,7 @@ export default function IllustrationGrid({ contents=[{url: "a"}, {url: "a"}], st
           <Link
             href={`/illustration/${illo.urlPath}`}
             key={illo.urlPath}
-            className="flex w-[max(300px,33%)] hover:scale-110 transition-transform duration-400 mb-8"
+            className={linkClasses}
           >
             <img
               src={illo.navigationImage.url}
