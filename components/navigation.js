b9e7891257a00005f45a47e02017f69451e19b07
charlie <charlie.a.fischer@gmail.com>
2023-10-05 20:00:15 -0700

    sticky nav

--- components/navigation.js
+++ components/navigation.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Navigation(){
   const [showModal, setModal] = useState(false)
   return (
-    <nav className="mx-4 flex justify-between items-center">
+    <nav className="mx-4 flex justify-between items-center sticky top-0">
       <Link href='/'>
         <h2>Charlie Fischer</h2>
       </Link>
